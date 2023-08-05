267c14ff44e6d08fc1665978371e5c849cbc1219
Thien-CG <lethien3072003@gmail.com>
2023-08-05 09:02:14 +0700

    trươc khi thay doi databas

--- StressWear/src/main/java/com/web/Controller/testController.java
+++ StressWear/src/main/java/com/web/Controller/testController.java
@@ -1,48 +1,48 @@
-package com.web.Controller;
+// package com.web.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class testController {
+// @Controller
+// public class testController {
 
-    @GetMapping("hello")
-    public String getPage() {
-        return "Manager/Manager-filter-page.html";
-    }
+//     @GetMapping("hello")
+//     public String getPage() {
+//         return "Manager/Manager-filter-page.html";
+//     }
 
-    @GetMapping("login")
-    public String getLogin() {
-        return "User-login-page";
-    }
+//     @GetMapping("login")
+//     public String getLogin() {
+//         return "User-login-page";
+//     }
 
-    // @GetMapping("index")
-    // public String index() {
-    //     return "User/User-index";
-    // }
+//     // @GetMapping("index")
+//     // public String index() {
+//     //     return "User/User-index";
+//     // }
 
-    @GetMapping("about")
-    public String about() {
-        return "User/User-about";
-    }
+//     @GetMapping("about")
+//     public String about() {
+//         return "User/User-about";
+//     }
 
-    @GetMapping("product")
-    public String product() {
-        return "User/User-product";
-    }
+//     @GetMapping("product")
+//     public String product() {
+//         return "User/User-product";
+//     }
 
-    @GetMapping("product-list")
-    public String productlist() {
-        return "User/User-product-List";
-    }
+//     @GetMapping("product-list")
+//     public String productlist() {
+//         return "User/User-product-List";
+//     }
 
-    @GetMapping("product-item")
-    public String productItem() {
-        return "User/User-product-Item";
-    }
+//     @GetMapping("contact")
+//     public String productItem() {
+//         return "User/User-contact";
+//     }
 
-    @GetMapping("/manager/filter")
-    public String ManagerFilter() {
-        return "Manager/Manager-filter-page";
-    }
-}
+//     @GetMapping("/manager/filter")
+//     public String ManagerFilter() {
+//         return "Manager/Manager-filter-page";
+//     }
+// }
