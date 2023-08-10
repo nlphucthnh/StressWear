158fa33c65f268d3d6fcf6ff89df6e1ce241d44d
chucphuong <117348246+chucphuong@users.noreply.github.com>
2023-08-11 03:24:59 +0700

    edit pro5

--- StressWear/src/main/java/com/web/Controller/User_Controller/UrLoginController.java
+++ StressWear/src/main/java/com/web/Controller/User_Controller/UrLoginController.java
@@ -5,10 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UrLoginController {
-    @RequestMapping("/user/changepass")
-    public String getChangePass(){
-        return "User/User-change-pass";
-    } 
+   
 
     @RequestMapping("/user/confirm")
     public String getConfirm(){
@@ -19,4 +16,10 @@ public class UrLoginController {
     public String getFogotPass(){
         return "User/User-forgot-pass";
     } 
+
+    // @RequestMapping("auth/sign/form")
+	// public String signInPage() {
+	// 	return "User/User-register-page";
+	// }
+    
 }
